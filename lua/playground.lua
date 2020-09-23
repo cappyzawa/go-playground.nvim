0ddad59c03ba3d4ce2d25863c05ee48e0a442983
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2020-09-23 14:15:20 +0900

    remove buffer flag

--- lua/playground.lua
+++ lua/playground.lua
@@ -2,7 +2,7 @@ local vim = vim
 local M = {}
 
 function M.setup()
-  vim.cmd [[command! -buffer -range=% -bang GoPlayground lua require'playground/share'.share(<line1>, <line2>)]]
+  vim.cmd [[command! -range=% -bang GoPlayground lua require'playground/share'.share(<line1>, <line2>)]]
 end
 
 return M

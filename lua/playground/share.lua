c6d540a23c1f7a53260648ffd3eccb639de5897a
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2020-09-25 11:33:04 +0900

    enable to use go2go playground

--- lua/playground/share.lua
+++ lua/playground/share.lua
@@ -3,23 +3,26 @@ local system = vim.fn.system
 local client = require'playground/client'
 local utils = require'playground/utils'
 local playground_url = "https://play.golang.org"
+local playground2_url = "https://go2goplay.golang.org"
 
-local M = {
-  share = function (s, e)
-    local browser_cmd = utils.browser()
+local M = {}
 
-    local bufnr = vim.api.nvim_get_current_buf()
-    local url = string.format("%s/share", playground_url)
-    local body = table.concat(vim.api.nvim_buf_get_lines(bufnr, s-1, e, true), "\n")
-    local out = client.post(url, body)
-    if vim.api.nvim_get_vvar("shell_error") == 0 then
-      local shared_url = string.format("%s/p/%s", playground_url, out)
-      print(shared_url)
-      system(string.format("%s %s", browser_cmd, shared_url))
-    else
-      error(out)
-    end
+function M.play (s, e, play2)
+  local browser_cmd = utils.browser()
+
+  local bufnr = vim.api.nvim_get_current_buf()
+  local url = playground_url
+  if play2 then url = playground2_url end
+  local share_url = string.format("%s/share", url)
+  local body = table.concat(vim.api.nvim_buf_get_lines(bufnr, s-1, e, true), "\n")
+  local out = client.post(share_url, body)
+  if vim.api.nvim_get_vvar("shell_error") == 0 then
+    local shared_url = string.format("%s/p/%s", url, out)
+    print(shared_url)
+    system(string.format("%s %s", browser_cmd, shared_url))
+  else
+    error(out)
   end
-}
+end
 
 return M
